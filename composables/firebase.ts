2da557ec7cfd878e38e5e3d7b1a017c129a193af
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-30 20:26:11 +0900

    debugtokenは常に有効でいい！

--- composables/firebase.ts
+++ composables/firebase.ts
@@ -38,7 +38,7 @@ const db = getFirestore(app);
 export const useDB = () => db;
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // @ts-ignore
-// self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
